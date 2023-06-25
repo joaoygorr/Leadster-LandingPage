2e84ee6dadfd94757665a098be521b6ecf73aeaa
joaoygorr <ygorvieira2002@gmail.com>
2023-06-25 15:28:32 -0400

    feat: navigation options

--- src/shared/components/MainContent/mainContent.tsx
+++ src/shared/components/MainContent/mainContent.tsx
@@ -1,12 +1,27 @@
 import Styles from "./mainContent.module.scss";
+import Data from "@/pages/api/data.json";
 
 export const MainContent = () => {
-    
+    const dataJson = Data.data
+
     return (
-        <nav>
-            <ul>
-                <li>main Content</li>
+        <nav className={`mx-auto w-full max-w-screen-xl ${Styles.boxContainer}`}>
+            <ul className={Styles.boxListButton}>
+                {dataJson.map((index, key) => {
+                    return (
+                        <li key={key}>
+                            <button>{index.label}</button>
+                        </li>
+                    )
+                })}
             </ul>
+
+            <div className={Styles.boxOrder}>
+                Ordernar por
+                <select>
+                    <option value="Data de Publicação">Data de Publicação</option>
+                </select>
+            </div>
         </nav>
     )
 }
