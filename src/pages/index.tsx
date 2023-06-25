4e01c3834b6f5f64fb0ba0c7002d75dafd10cf69
joaoygorr <ygorvieira2002@gmail.com>
2023-06-24 22:24:04 -0400

    feat: add sectionContent component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import { Header } from "@/shared/components/Header/header";
+import { SectionContent } from "@/shared/components/SectionContent/sectionContent";
+import Styles from "./index.module.scss";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <main>
+        <SectionContent />
+      </main>
     </div>
   )
 }
