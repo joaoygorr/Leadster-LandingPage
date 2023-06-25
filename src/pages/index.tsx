d0ba047690c83fadf9cefd323195dfc8b7483145
joaoygorr <ygorvieira2002@gmail.com>
2023-06-25 02:23:34 -0400

    feat: import component SectionFooter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
 import { Header } from "@/shared/components/Header/header";
-import { SectionContent } from "@/shared/components/SectionContent/sectionContent";
-import Styles from "./index.module.scss";
+import { SectionHeader } from "@/shared/components/SectionHeader/sectionHeader";
+import { Footer } from "@/shared/components/Footer/footer";
+import { MainContent } from "@/shared/components/MainContent/mainContent";
+import { SectionFooter } from "@/shared/components/SectionFooter/sectionFooter";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <main>
-        <SectionContent />
+        <SectionHeader />
+        <MainContent />
+        <SectionFooter />
       </main>
+      <Footer />
     </div>
   )
 }
