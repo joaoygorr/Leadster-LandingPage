a1c9f4fa1cbb1ae121e82f9ee8456e3f3c3a618b
joaoygorr <ygorvieira2002@gmail.com>
2023-06-25 02:23:47 -0400

    feat: new mainComponent

---
+++ src/shared/components/MainContent/mainContent.tsx
@@ -0,0 +1,12 @@
+import Styles from "./mainContent.module.scss";
+
+export const MainContent = () => {
+    
+    return (
+        <nav>
+            <ul>
+                <li>main Content</li>
+            </ul>
+        </nav>
+    )
+}
