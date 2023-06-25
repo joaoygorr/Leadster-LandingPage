c54ccfe17b7f965d6eb62f1b307513a9e76ca494
joaoygorr <ygorvieira2002@gmail.com>
2023-06-25 03:03:15 -0400

    fix: responsividade

--- src/shared/components/SectionFooter/sectionFooter.tsx
+++ src/shared/components/SectionFooter/sectionFooter.tsx
@@ -8,8 +8,8 @@ import Rating from "@/assets/image/rating.webp";
 export const SectionFooter = () => {
     return (
         <section className={`${Styles.container} mb-10`}>
-            <div className={`grid grid-cols-2 p-5 mx-auto w-full max-w-screen-xl ${Styles.boxContent}`}>
-                <div className="boxImage">
+            <div className={`grid lg:grid-cols-2 md:grid-cols-1 p-5 mx-auto w-full max-w-screen-xl ${Styles.boxContent}`}>
+                <div className="flex justify-center items-center">
                     <Image src={Graph} alt="gráfico" />
                 </div>
                 <div className={Styles.boxInfos}>
@@ -21,7 +21,7 @@ export const SectionFooter = () => {
                         <button>VER DEMONSTRAÇÃO</button>
                         <Image src={Selo} alt="Selo" />
                     </div>
-                    <div className="flex items-center">
+                    <div className="flex lg:items-center lg:justify-start md:justify-center md:items-center">
                         <Image src={Card} alt="no-card" /><strong className="mx-1">Não</strong> é necessário Cartão de Crédito | <Image src={Rating} alt="avaliação" className="mx-2" /> <strong>4.9</strong>/5 de satisfação
                     </div>
                 </div>
