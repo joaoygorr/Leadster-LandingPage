0b985283622d62e9a3dbca335436b471eabf9d33
joaoygorr <ygorvieira2002@gmail.com>
2023-06-24 20:59:32 -0400

    feat: header component

---
+++ src/shared/components/Header/header.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import LogoLeadster from "@/assets/image/logo.png";
+import Styles from "./header.module.scss";
+
+export const Header = () => {
+  return (
+    <header className={Styles.container}>
+      <Image
+        src={LogoLeadster}
+        alt="Logo leadster"
+        className={Styles.img}
+      />
+    </header>
+  )
+};
